84f70bb24734e1bad3e6c20c72a23aa40b09640b
Michael Walle <michael@walle.cc>
2018-10-16 15:09:31 +0200

    pid: major update
    Make derivative part a compile time option and use int16_t for the
    integrator. Remove max_error check, because internally, we calculate
    with 32 bit now. Remove float data types.
    
    Signed-off-by: Michael Walle <michael@walle.cc>

--- firmware/pid.c
+++ firmware/pid.c
@@ -2,58 +2,81 @@
 #include <stdint.h>
 #include "pid.h"
 
-#define PID_SCALING_FACTOR 32
+struct pid {
+	int16_t p_gain;
+	int16_t i_gain;
+	int16_t band;
+	//int16_t max_error;
+
+	int16_t i_sum;
+	int16_t i_min;
+	int16_t i_max;
+
+	//int16_t kt;
+	//int16_t out_min;
+	//int16_t out_max;
+	//int16_t out_last;
+
+#ifdef PID_D
+	int16_t d_gain;
+	int16_t d_last;
+#endif
+};
 
 struct pid _pid, *pid = &_pid;
 
-void pid_init(float p_gain, float i_gain, float d_gain,
-		float i_min, float i_max, int16_t band)
+void pid_init(int16_t p_gain, int16_t i_gain, int16_t d_gain,
+		int32_t i_min, int32_t i_max, int16_t band)
 {
-	pid->p_gain = p_gain * PID_SCALING_FACTOR;
-	pid->i_gain = i_gain * PID_SCALING_FACTOR;
-	pid->d_gain = d_gain * PID_SCALING_FACTOR;
+	pid->p_gain = p_gain;
 	pid->band = band;
+	//pid->max_error = INT16_MAX / (pid->p_gain + 1);
 
-	pid->i_sum = 0;
-	pid->d_last = INT16_MIN;
-
+	pid->i_gain = i_gain;
 	pid->i_min = i_min;
 	pid->i_max = i_max;
-	pid->kt = 5;
 
-	pid->max_error = INT16_MAX / (pid->p_gain + 1);
-	pid->out_last = 0;
-	pid->out_min = 0;
-	pid->out_max = 200;
+	//pid->kt = 5;
+	//pid->out_min = 0;
+	//pid->out_max = 200;
+
+#ifdef PID_D
+	pid->d_gain = d_gain;
+#endif
+
+	pid_reset();
 }
 
 void pid_reset(void)
 {
 	pid->i_sum = 0;
+	//pid->out_last = 0;
+#ifdef PID_D
 	pid->d_last = INT16_MIN;
-	pid->out_last = 0;
+#endif
 }
 
 int16_t pid_update(int16_t error, int16_t position)
 {
-	int16_t p, d;
-	int32_t ret, i;
+	int32_t ret, p, i, d;
 
 	if (error < -pid->band)
 		return INT16_MIN;
 	else if (error > pid->band)
 		return INT16_MAX;
 
+#if 0
 	/* TBD limit error to prevent overflow */
 	if (error > pid->max_error)
 		error = pid->max_error;
 	else if (error < -pid->max_error)
 		error = -pid->max_error;
+#endif
 
-	p = pid->p_gain * error;
+	p = (int32_t)pid->p_gain * error;
 
 	/* account error within limits */
-	pid->i_sum += error;
+	i = (int32_t)pid->i_sum + error;
 #if 0
 	if (pid->out_last > pid->out_max) {
 		//printf("remove %d..", pid->i_sum);
@@ -65,21 +88,26 @@ int16_t pid_update(int16_t error, int16_t position)
 		//printf("%d\n", pid->i_sum);
 	}
 #else
-	if (pid->i_sum > pid->i_max)
+	if (i > pid->i_max)
 		pid->i_sum = pid->i_max;
-	else if (pid->i_sum < pid->i_min)
+	else if (i < pid->i_min)
 		pid->i_sum = pid->i_min;
+	else
+		pid->i_sum = i;
 #endif
-
 	i = pid->i_gain * pid->i_sum;
 
+#if PID_D
 	if (pid->d_last != INT16_MIN)
 		d = pid->d_gain * (pid->d_last - position);
 	else
 		d = 0;
 	pid->d_last = position;
+#else
+	d = 0;
+#endif
 
-	ret = (p + i + d) / PID_SCALING_FACTOR;
+	ret = (p + i + d) >> PID_SCALING_SHIFT;
 
 	//printf("PID %d %f %f %f ret=%ld\r\n", error, (float)p/32, (float)i/32, (float)d/32, ret);
 	if (ret > INT16_MAX)
@@ -87,6 +115,6 @@ int16_t pid_update(int16_t error, int16_t position)
 	else if (ret < INT16_MIN)
 		ret = INT16_MIN;
 
-	pid->out_last = ret;
+	//pid->out_last = ret;
 	return ret;
 }
--- firmware/pid.h
+++ firmware/pid.h
@@ -1,29 +1,10 @@
 #ifndef __PID_H
 #define __PID_H
 
-struct pid {
-	int16_t p_gain;
-	int16_t i_gain;
-	int16_t d_gain;
-	int16_t band;
+#define PID_SCALING_SHIFT 10
 
-	int32_t i_sum;
-	int16_t d_last;
-	int16_t kt;
-
-	int32_t i_min;
-	int32_t i_max;
-	int16_t max_error;
-
-	int16_t out_min;
-	int16_t out_max;
-	int16_t out_last;
-};
-
-extern struct pid *pid;
-
-void pid_init(float p_gain, float i_gain, float d_gain,
-		float i_min, float i_max, int16_t band);
+void pid_init(int16_t p_gain, int16_t i_gain, int16_t d_gain,
+		int32_t i_min, int32_t i_max, int16_t band);
 int16_t pid_update(int16_t error, int16_t position);
 void pid_reset(void);
 
