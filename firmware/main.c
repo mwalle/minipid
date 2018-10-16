19adab67161d47a4cfe4b648b3469c4bab60aa50
Michael Walle <michael@walle.cc>
2018-10-16 13:45:28 +0200

    adc: convert to fixed point arithmetic
    Signed-off-by: Michael Walle <michael@walle.cc>

--- firmware/main.c
+++ firmware/main.c
@@ -8,11 +8,14 @@
 
 #include "usi.h"
 
-#define ADC_VAL(x) (1000UL * 0x1fff / ((x) + 1000))
-static const int16_t pt1000_tab[][2] PROGMEM = {
-	{0,   ADC_VAL(1000)}, // 1023
-	{100,  ADC_VAL(1039)}, // 1003
-	{200,  ADC_VAL(1078)}, //  985
+#define ADC_VAL(x) (0x1000UL * (x) / ((x) + 1000))
+static const struct {
+	const int16_t t;
+	const uint16_t v;
+} pt1000_tab[] PROGMEM = {
+	{0,    ADC_VAL(1000)}, // 2048  -> 0
+	{100,  ADC_VAL(1039)},
+	{200,  ADC_VAL(1078)},
 	{300,  ADC_VAL(1117)},
 	{400,  ADC_VAL(1155)},
 	{500,  ADC_VAL(1194)},
@@ -26,23 +29,41 @@ static const int16_t pt1000_tab[][2] PROGMEM = {
 	{1300, ADC_VAL(1498)},
 	{1400, ADC_VAL(1536)},
 	{1500, ADC_VAL(1573)},
-	{0  ,  ADC_VAL(1611)},
+	{1600, ADC_VAL(1611)},
+	{1700, ADC_VAL(1648)}, // 2549 -> 501
 };
 
-int16_t adc2degc(int16_t val)
+#define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
+#define PT1000_VMIN (pt1000_tab[0].v)
+#define PT1000_VMAX (pt1000_tab[ARRAY_SIZE(pt1000_tab)-1].v)
+#define PT1000_TMIN (pt1000_tab[0].t)
+#define PT1000_TMAX (pt1000_tab[ARRAY_SIZE(pt1000_tab)-1].t)
+
+static int16_t adc2degc(uint16_t val)
 {
 	uint8_t i = 0;
-	int16_t temp;
-	float t1, t2, v1, v2;
+	uint32_t temp;
+	uint16_t t1, t2, v1, v2;
 
-	while ((v2 = pgm_read_word(&(pt1000_tab[i][1]))) > val)
+	if (val <= PT1000_VMIN)
+		return PT1000_TMIN;
+	if (val >= PT1000_VMAX)
+		return PT1000_TMAX;
+
+	while ((v2 = pgm_read_word(&(pt1000_tab[i].v))) <= val)
 		i++;
 
-	v1 = pgm_read_word(&(pt1000_tab[i-1][1]));
-	t1 = pgm_read_word(&(pt1000_tab[i-1][0]));
-	t2 = pgm_read_word(&(pt1000_tab[i][0]));
+	v1 = pgm_read_word(&(pt1000_tab[i-1].v));
+	t1 = pgm_read_word(&(pt1000_tab[i-1].t));
+	t2 = pgm_read_word(&(pt1000_tab[i].t));
 
-	temp = t1 + (t2 - t1) / (v2 - v1) * ((float)val - v1);
+	/* temp = t1 + (t2 - t1) / (v2 - v1) * (val - v1) */
+	temp = (t2 - t1);
+	temp <<= 16;
+	temp /= (v2 - v1);
+	temp *= (val - v1);
+	temp >>= 16;
+	temp += t1;
 
 	return temp;
 }
